cbe1d422fa9e38a041a5cbe688163f25fe3c8e5f
Gustavo Henrique <gustavohffs@gmail.com>
2023-11-08 20:52:34 -0300

    First Model-Controller-View - Department

--- SalesWebMvc2/Controllers/DepartmentsController.cs
+++ SalesWebMvc2/Controllers/DepartmentsController.cs
@@ -12,7 +12,7 @@ namespace SalesWebMvc2.Controllers
         public IActionResult Index()
         {
             List<Department> list = new List<Department>();
-            list.Add(new Department { Id = 1, Name = "Computers" });
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
             list.Add(new Department { Id = 2, Name = "Fashion" });
 
             return View(list);
