f0e7d9c5f4c94af328bd737b960859c5ef42b558
Gustavo Henrique <gustavohffs@gmail.com>
2023-11-08 16:01:32 -0300

    Project CreatedWrong

---
+++ SalesWebMvc2/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc2.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
