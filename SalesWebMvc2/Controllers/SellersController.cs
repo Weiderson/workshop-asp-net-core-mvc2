bc98896256734242615d5400be0be9d54f033eba
Gustavo Henrique <gustavohffs@gmail.com>
2023-11-09 15:50:41 -0300

    Simple Create form

--- SalesWebMvc2/Controllers/SellersController.cs
+++ SalesWebMvc2/Controllers/SellersController.cs
@@ -24,5 +24,18 @@ namespace SalesWebMvc2.Controllers
 
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- SalesWebMvc2/Services/SellerService.cs
+++ SalesWebMvc2/Services/SellerService.cs
@@ -20,5 +20,11 @@ namespace SalesWebMvc2.Models
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
