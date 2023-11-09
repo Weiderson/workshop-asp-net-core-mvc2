0a61d5ec173f92b8e8e4f86d0b66d2211b0962aa
Gustavo Henrique <gustavohffs@gmail.com>
2023-11-08 21:43:47 -0300

    CRUD Scaffolding

---
+++ SalesWebMvc2/Models/Enums/SaleStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc2.Models.Enums
+{
+    public enum Status : int
+    {
+        Canceled = 0,
+        Billed = 1,
+        Shiped = 2
+    }
+}
---
+++ SalesWebMvc2/Models/SalesRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc2.Models
+{
+    public class SalesRecord
+    {
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+
+    }
+}
